4126267eef11a8f94ccc4a68146bb1fd15945456
JoshuaBrockschmidt <JoshuaBrockschmidt@gmail.com>
2015-06-27 20:04:26 -0700

    Pull image data from Leap Motion Controller

--- leapmotion/src/server.py
+++ leapmotion/src/server.py
@@ -1,8 +1,8 @@
 # This script recieves data from the Leap Motion Controller, interprets it, and
 # sends the coordinates of a red dot to a client.
 
-import ctypes
-import os, sys, inspect
+import ctypes, os, sys, inspect
+from PIL import Image
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
@@ -20,30 +20,42 @@ class CustomListener(Leap.Listener):
 
     def on_images(self, ctrl):
         print "Image gotten"
-        print ctrl.images[0].data
-        sys.exit()
+        for k, img in enumerate(ctrl.images):
+            if not img.is_valid:
+                print "Well f***"
+                continue
+            imageData = ctypes.cast(
+                img.data.cast().__long__(),
+                ctypes.POINTER(img.width*img.height*ctypes.c_ubyte)
+            ).contents
+            newImg = Image.frombuffer(
+                "L", (img.width, img.height),
+                imageData, "raw", "L", 0, 1
+            )
+            newImg.save("test" + str(k) + ".png")
+            print "test" + str(k) + ".png created"
+        os._exit(1)
 
     def on_frame(self, ctrl):
         print "Frame gotten"
-        frame = ctrl.frame()
-        img = frame.images[0]
-        if img.is_valid:
-            array_def = ctypes.c_ubyte * img.width * img.height
-            array_data = array_def.from_address(int(img.data_pointer))
-            print("Worked?")
-            os._exit(1)
 
 def main():
     ctrl = Leap.Controller()
-    print ctrl.is_policy_set(Leap.Controller.POLICY_IMAGES)
-    print ctrl.is_policy_set(Leap.Controller.POLICY_BACKGROUND_FRAMES)
-    print ctrl.is_policy_set(Leap.Controller.POLICY_OPTIMIZE_HMD)
+    print("POLICY_IMAGES:            " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_IMAGES)) )
+    print("POLICY_BACKGROUND_FRAMES: " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_BACKGROUND_FRAMES)) )
+    print("POLICY_OPTIMIZE_HMD:      " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_OPTIMIZE_HMD)) )
     ctrl.set_policy(Leap.Controller.POLICY_BACKGROUND_FRAMES)
     ctrl.set_policy(Leap.Controller.POLICY_IMAGES)
     ctrl.set_policy(Leap.Controller.POLICY_OPTIMIZE_HMD)
-    print ctrl.is_policy_set(Leap.Controller.POLICY_IMAGES)
-    print ctrl.is_policy_set(Leap.Controller.POLICY_BACKGROUND_FRAMES)
-    print ctrl.is_policy_set(Leap.Controller.POLICY_OPTIMIZE_HMD)
+    print("POLICY_IMAGES:            " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_IMAGES)) )
+    print("POLICY_BACKGROUND_FRAMES: " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_BACKGROUND_FRAMES)) )
+    print("POLICY_OPTIMIZE_HMD:      " +
+          str(ctrl.is_policy_set(Leap.Controller.POLICY_OPTIMIZE_HMD)) )
     lstn = CustomListener()
     ctrl.add_listener(lstn)
 
